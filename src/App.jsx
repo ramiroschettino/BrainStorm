a28aad9a04a1e3ffdf6d9d383e4792829bebaf69
Ramiro Schettino <ramiroschettino1@hotmail.com>
2024-07-06 22:12:59 -0300

    haciendo la header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-// import ButtonGradient from "./assets/svg/ButtonGradient";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 import { Header } from "./components/Header";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header></Header>
+        <Header />
       </div>
-      {/* <ButtonGradient /> */}
+      <ButtonGradient />
     </>
   );
 };
